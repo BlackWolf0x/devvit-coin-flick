2abb4405de9038159f76bc766dfac0dce5ba7b34
Shahen <shahenalgoo@gmail.com>
2026-02-10 21:28:37 +0400

    Update ChestOpen.tsx

--- src/client/pages/ChestOpen.tsx
+++ src/client/pages/ChestOpen.tsx
@@ -4,7 +4,7 @@ export default function ChestOpen() {
 	const goBack = useNavigationStore((state) => state.goBack);
 
 	return (
-		<div className="relative h-screen bg-background pt-6 flex flex-col gap-4 px-4 pb-4">
+		<div className="relative h-screen pt-6 flex flex-col gap-4 px-4 pb-4">
 			<button
 				onClick={goBack}
 				className="absolute top-4 left-4 text-white hover:opacity-80 transition-opacity"
