44c8318b4f358e5a47de89ad7257b20b7d845666
Shahen <shahenalgoo@gmail.com>
2026-02-11 14:16:23 +0400

    fixed desktop version

--- src/client/pages/Splash.tsx
+++ src/client/pages/Splash.tsx
@@ -109,15 +109,15 @@ export default function Splash() {
 
 	return (
 		<div className="relative h-screen overflow-hidden flex flex-col justify-between">
-			<div className="absolute top-4 left-2 scale-90">
+			<div className="absolute top-4 left-2 notmobile:top-6 notmobile:left-4 scale-90 notmobile:scale-100">
 				<MyBalance displayBalance={displayBalance} />
 			</div>
 
-			<div className="absolute top-14 left-2 scale-75">
+			<div className="absolute top-14 left-2 notmobile:top-20 notmobile:left-6 scale-75 notmobile:scale-100">
 				<OpenChestSplash displayBalance={displayBalance} />
 			</div>
 
-			<div className="absolute top-2 right-2 space-y-2 scale-90">
+			<div className="absolute top-2 right-2 space-y-2 notmobile:top-6 notmobile:right-4 notmobile:space-y-3 scale-90 notmobile:scale-100">
 				<TopRightButtons />
 			</div>
 
@@ -128,7 +128,7 @@ export default function Splash() {
 				/>
 			</div>
 
-			<div className="mb-6 w-3/4 mx-auto space-y-2">
+			<div className="mb-6 notmobile:mb-8 w-3/4 notmobile:w-80 mx-auto space-y-2">
 				<DailyChallenges allChallenges={displayChallenges} />
 			</div>
 
