ed97f1fb71b21a261a82e1b39d4533456c9a08fb
Shahen <shahenalgoo@gmail.com>
2026-02-11 08:45:53 +0400

    open chest client test

--- src/client/pages/ChestOpen.tsx
+++ src/client/pages/ChestOpen.tsx
@@ -1,7 +1,25 @@
 import { useNavigationStore } from '@/stores/navigationStore';
+import { useMutation } from '@tanstack/react-query';
+
+const openChest = async () => {
+	const response = await fetch('/api/open-chest', {
+		method: 'POST',
+	});
+	if (!response.ok) {
+		throw new Error(`HTTP error! status: ${response.status}`);
+	}
+	const data = await response.json();
+	console.log(data);
+	return data;
+};
 
 export default function ChestOpen() {
 	const goBack = useNavigationStore((state) => state.goBack);
+
+	// Player count update mutation
+	const openChestMutation = useMutation({
+		mutationFn: openChest,
+	});
 
 	return (
 		<div className="relative h-screen pt-6 flex flex-col gap-4 px-4 pb-4">
@@ -13,6 +31,8 @@ export default function ChestOpen() {
 			</button>
 			<div className="flex-1 flex items-center justify-center">
 				<h1 className="text-2xl text-white">Chest Open Page</h1>
+
+				<button onClick={() => openChestMutation.mutate()}>Open Chest</button>
 			</div>
 		</div>
 	);
--- src/server/api/index.ts
+++ src/server/api/index.ts
@@ -3,6 +3,8 @@ import { Router } from 'express';
 import playerCount from './player-count';
 import playerCountUpdate from './player-count-update';
 import userData from './user-data';
+import openChest from './open-chest';
+
 import awardReward from './reward';
 
 const router = Router();
@@ -11,6 +13,8 @@ const router = Router();
 router.use(playerCount);
 router.use(playerCountUpdate);
 router.use(userData);
+router.use(openChest);
+
 router.use(awardReward);
 
 export default router;
